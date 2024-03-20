4705a48c64b6e5416773aab31e56d830fd1116d7
NUS16248-12-aletuch <aletuch@nuevaschool.org>
2024-03-20 10:10:58 -0700

    syncing 3/20

--- Detector.js
+++ Detector.js
@@ -1,14 +1,7 @@
 import * as FileSystem from 'expo-file-system';
 import React, {Component} from 'react';
 
-//TEST THIS FILE
-
-/*
-
-Next Steps 3/18: 
-- Find form of binary audio data - see if workable
-
-*/
+//Detatch from reacat expo 
 
 export default class Detector extends Component{
     constructor(){
@@ -20,30 +13,7 @@ export default class Detector extends Component{
     async loadAudio(recording){
         console.log("in Load Audio");
 
-        //load binary audio data (workaround for audio buffer)
 
-        try {
-            const fileUri = await FileSystem.downloadAsync(recording.uri, `${FileSystem.documentDirectory}/recording.mp4`);
-            console.log("finished downloading!");
-
-            const fileInfo = await FileSystem.getInfoAsync(fileUri);
-
-            if (!fileInfo.exists) {
-              console.error('File does not exist');
-              return;
-            }
-            const audioData = await FileSystem.readAsStringAsync(fileUri, { encoding: FileSystem.EncodingType.Base64 });
-          
-            const byteCharacters = atob(audioData);
-            const byteNumbers = new Array(byteCharacters.length);
-            for (let i = 0; i < byteCharacters.length; i++) {
-              byteNumbers[i] = byteCharacters.charCodeAt(i);
-            }
-            console.log("data: ", new Uint8Array(byteNumbers));
-          
-        } catch (error) {
-            console.error('Failed to fetch audio data', error);
-        }
     }
 }
 
